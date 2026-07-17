d639908d0d91f23c2c4c63827c7c5e586c17b85e
eliki2003 <eliana.terrones1@unmsm.edu.pe>
2026-07-17 14:32:07 -0500

    Actualizar modelo Gemini

--- src/app/projects/emi/infra/clients/llm.py
+++ src/app/projects/emi/infra/clients/llm.py
@@ -1,15 +1,16 @@
 import os
 import google.generativeai as genai
+from dotenv import load_dotenv
 
+load_dotenv("credentials/emi.env")
 
 genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-3.5-flash")
 
 
 async def generate_response(prompt: str) -> str:
     response = await model.generate_content_async(prompt)
-
-    return response.text.strip()
+    return response.text
